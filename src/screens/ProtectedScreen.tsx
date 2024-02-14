d10652985d53cd60fb99871a85632506e85449eb
fmoraless <franticzero@gmail.com>
2024-02-14 15:50:39 -0300

    react navigation implementation

---
+++ src/screens/ProtectedScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const ProtectedScreen = () => {
+  return (
+    <View>
+      <Text>ProtectedScreen</Text>
+    </View>
+  );
+};
