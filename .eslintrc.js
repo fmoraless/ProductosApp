197a247453f0f1d3a854cc92d3368f656c5bedb5
fmoraless <franticzero@gmail.com>
2024-02-15 12:26:15 -0300

    chore: eslint, prettier and husky installations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'jest'],
+  parser: '@typescript-eslint/parser',
+  /* rules: {
+    'prettier/prettier': 0,
+  }, */
+  rules: {
+    'prettier/prettier': 0,
+    'react/jsx-uses-react': 1,
+  },
 };
